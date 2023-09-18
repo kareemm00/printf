ca3b53995ed7c516cba017f6138c5c0758213d4b
kareemm00 <karimmohamed4624@gmail.com>
2023-09-18 16:31:00 -0700

    strlen

---
+++ _strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ *
+ * @s: parameter string
+ *
+ * Return: length of a string
+*/
+
+int _strlen(char*s)
+{
+	int length;
+	int count = 0;
+
+	for (length = 0; s[length] != '\0'; s++)
+	{
+		count += _putchar(s[length]);
+	}
+	return (count);
+}
+
