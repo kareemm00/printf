958b9b208b4aad818b92507eaa5e8c58aced2154
ahmedelmkawey <ahmedmkaway@gmail.com>
2023-09-19 19:14:00 -0700

    fix3

--- _printf.c
+++ _printf.c
@@ -8,40 +8,18 @@ int _printf(const char *format, ...)
 {
 	int count = 0;
 	va_list arguments;
-	char *str;
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 	va_start(arguments, format);
-	while (*format)
+	while (format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			switch (*format)
-			{
-				case 'c':
-					count += _putchar(va_arg(arguments, int));
-					break;
-				case 's':
-					str = (va_arg(arguments, char *));
-					if (str == NULL)
-					{
-						str = "(null)";
-						write(1, str, 6);
-						count += 6;
-					}
-					else
-						count += _strlen(va_arg(arguments, char *));
-					break;
-				case '%':
-					count += _putchar('%');
-					break;
-				default:
-					_putchar(*format); 
-			} 
+			count += checker(format, arguments);
 		}
 		else
 			count += _putchar(*format);
--- main.h
+++ main.h
@@ -2,13 +2,12 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <stdio.h>
 #include <unistd.h>
 
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _strlen(char *s);
-
+int checker(const char *format, va_list arguments);
 
 #endif
